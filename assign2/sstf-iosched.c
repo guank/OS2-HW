f8dd238e49935bbe83f98bd80e40880eb84b80bb
guank <guank@users.noreply.github.com>
2017-05-01 17:06:59 -0700

    Add files via upload

---
+++ assign2/sstf-iosched.c
@@ -0,0 +1,10 @@
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
+#include <linux/bio.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+struct sstf_data {
+	struct list_head queue;
+};
